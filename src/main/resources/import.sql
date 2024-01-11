46f999a814f6b16dcbb7c94814ef862d1dc4b77e
Joao Pedro <jaopedrobarbosa15@gmail.com>
2024-01-11 14:25:33 -0400

    Conexao Banco de dados e seed dos dados

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_user (name, cpf, income, children, birth_date) VALUES ('Maria eduarda', '12345678901', '1200', '0', '2001-07-25');
+INSERT INTO tb_user (name, cpf, income, children, birth_date) VALUES ('Joao Pedro', '12376578912', '2500', '2', '2000-02-08');
+INSERT INTO tb_user (name, cpf, income, children, birth_date) VALUES ('Luiz Felipe', '91345678933', '3100.50', '1', '1999-05-13');
+INSERT INTO tb_user (name, cpf, income, children, birth_date) VALUES ('Marcos', '10495678931', '1800', '0', '2000-08-30');
+INSERT INTO tb_user (name, cpf, income, children, birth_date) VALUES ('Iago', '85342678925', '4000', '2', '1995-09-15');
+INSERT INTO tb_user (name, cpf, income, children, birth_date) VALUES ('Julia', '12345675843', '2200', '1', '2002-01-21');
+INSERT INTO tb_user (name, cpf, income, children, birth_date) VALUES ('Samuel', '19475878978', '7000', '3', '1990-11-27');
+INSERT INTO tb_user (name, cpf, income, children, birth_date) VALUES ('Matheus', '10846578976', '3800', '0', '1993-06-10');
+INSERT INTO tb_user (name, cpf, income, children, birth_date) VALUES ('Felipe', '12345678345', '2900', '1', '2000-12-09');
+INSERT INTO tb_user (name, cpf, income, children, birth_date) VALUES ('Fernando', '91476298461', '3500', '1', '2002-04-05');
+
